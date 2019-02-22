2bce051641a174366e96b19703d7f2aa7df86612
carynsmith01 <47799419+carynsmith01@users.noreply.github.com>
2019-02-22 15:25:57 -0500

    Add files via upload

--- edsby.c
+++ edsby.c
@@ -1,11 +1,16 @@
-#include <stdio.j>
+#include <stdio.h>
 #include <cs50.h>
  int POINTSPOSSIBLE;
  int POINTSEARNED;
  int ASSIGNMENT;
 
 int main (void){
-    int POINTSPOSSIBLE=get_int("How many possible points?");
-        POINTSPOSSIBLE=POINTSPOSSIBLE+i
-    int POINTSEARNED=
+    int i=ASSIGHNMENT
+    ASSIGNMENT=get_int("How many assignments?");
+        for(i=0;i<ASSIGNMENT;i++){
+    POINTSPOSSIBLE=get_int("How many possible points?");
+        POINTSPOSSIBLE=POINTSPOSSIBLE+i;
+    POINTSEARNED=get_int("How many points earned?");
+        POINTSEARNED=POINTSEARNED+i;
+}
 }
