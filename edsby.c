89b9321e7e7d76d7f3ff10b160e84be1dde68df0
carynsmith01 <47799419+carynsmith01@users.noreply.github.com>
2019-02-22 23:57:30 -0500

    Add files via upload

--- edsby.c
+++ edsby.c
@@ -1,16 +1,20 @@
 #include <stdio.h>
+
 #include <cs50.h>
- int POINTSPOSSIBLE;
- int POINTSEARNED;
- int ASSIGNMENT;
+int POINTSPOSSIBLE;
+int POINTSEARNED;
+int ASSIGNMENT;
 
 int main (void){
-    int i=ASSIGHNMENT
-    ASSIGNMENT=get_int("How many assignments?");
-        for(i=0;i<ASSIGNMENT;i++){
-    POINTSPOSSIBLE=get_int("How many possible points?");
-        POINTSPOSSIBLE=POINTSPOSSIBLE+i;
-    POINTSEARNED=get_int("How many points earned?");
-        POINTSEARNED=POINTSEARNED+i;
+    int i=ASSIGNMENT;
+      ASSIGNMENT=get_int("How many assignments?");// Ask the user for an integer 
+    for(i=0;i<ASSIGNMENT;i++){//runs the program as long as the given integer is less than the max number of assignments
+    POINTSPOSSIBLE=get_int("How many possible points?");//get another integer from the user to represent the number of possible points
+      POINTSPOSSIBLE=POINTSPOSSIBLE+i;
+    POINTSEARNED=get_int("How many points earned?");//get a last integer from the user to describe the amount of points earned 
+      POINTSEARNED=POINTSEARNED+i;
+    int score=POINTSEARNED/POINTSPOSSIBLE;//use score to represent the points earned out of the points possible
+        printf("score:%d\n",i);//print the score of each output from line 16
+    }
 }
-}
+
